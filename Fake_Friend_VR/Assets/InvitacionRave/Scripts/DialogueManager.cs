b218029663d9b1cc359e50b95b548cda90f765bb
pedro-merida <pmerida99@gmail.com>
2023-09-10 18:02:36 -0300

    Agregado InvitacionRave

---
+++ Fake_Friend_VR/Assets/InvitacionRave/Scripts/DialogueManager.cs
@@ -0,0 +1,279 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Dreamteck.Splines;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+namespace DS
+{
+    using ScriptableObjects;
+    public class DialogueManager : MonoBehaviour
+    {
+        public GameObject dialogueBoxFenia;
+        public GameObject dialogueBoxLuis;
+        public GameObject Luis;
+        public GameObject Fenia;
+        public GameObject LevelLoader;
+        private Animator LuisController;
+        private Animator FeniaController;
+
+        [Tooltip("Actions to check")]
+        public InputAction action = null;
+
+        //public Text actorName;
+        //public Text messageText;
+        //public RectTransform backgroundBox;
+
+        Message[] currentMessages;
+        Actor[] currentActors;
+        int activeMessage = 0;
+        int decisiones;
+        public static bool isActive = false;
+        public GameObject[] options;
+        public GameObject hablar;
+        string actor_anterior;
+        DSDialogueSO currentDialogue;
+        /*public void OpenDialogue(Message[] messages, Actor[] actors)
+        {
+            currentMessages = messages;
+            currentActors = actors;
+            activeMessage = 0;
+
+            isActive = true;
+            Debug.Log("TEST Comenzó la conversación, se han cargado " + messages.Length);
+            Debug.Log(currentMessages[activeMessage].message);
+            DisplayMessage();
+        }*/
+
+        private void Awake()
+        {
+            action.started += Pressed;
+        }
+
+        private void OnDestroy()
+        {
+            action.started -= Pressed;
+        }
+
+        private void OnEnable()
+        {
+            action.Enable();
+        }
+
+        private void OnDisable()
+        {
+            action.Disable();
+        }
+
+        private void Pressed(InputAction.CallbackContext context)
+        {
+            if (isActive)
+            {
+                Debug.Log("Acabas de apretar espacio");
+                NextMessage();
+            }
+        }
+
+
+        public void OpenDialogue(DSDialogueSO dialogue)
+        {
+            hablar.SetActive(false);
+            currentDialogue = dialogue;
+            activeMessage = 0;
+            isActive = true;
+            //Debug.Log("TEST Comenzó la conversación, se han cargado " + messages.Length);
+
+            DisplayMessage();
+        }
+
+
+        void DisplayMessage()
+        {
+
+            string dialogo = currentDialogue.Text;
+            string actor = currentDialogue.Actor;
+            actor_anterior = actor;
+            if (actor == "Feña")
+            {
+                //Abrir dialogo de Fena
+                //GameObject parent = GameObject.FindGameObjectsWithTag("Feña_Dialogo")[0];
+                GameObject parent = dialogueBoxFenia;
+                //parent.SetActive(true);
+
+                GameObject actorName = parent.transform.Find("Actor").gameObject;
+                GameObject messageText = parent.transform.Find("Message").gameObject;
+                GameObject backgroundBox = parent;
+                backgroundBox.LeanScale(Vector3.one, 0.5f);
+
+                messageText.GetComponent<UnityEngine.UI.Text>().text = dialogo;
+                //Actor actorToDisplay = currentActors[messageToDisplay.actorId];
+                actorName.GetComponent<UnityEngine.UI.Text>().text = actor;
+
+                LeanTween.textAlpha(messageText.GetComponent<UnityEngine.UI.Text>().rectTransform, 0, 0);
+                LeanTween.textAlpha(messageText.GetComponent<UnityEngine.UI.Text>().rectTransform, 1, 0.5f);
+
+            }
+            else if (actor == "Luis")
+            {
+                //Abrir dialogo de Luis
+                //GameObject parent = GameObject.FindGameObjectsWithTag("Luis_Dialogo")[0];
+                GameObject parent = dialogueBoxLuis;
+                //parent.SetActive(true);
+
+                GameObject actorName = parent.transform.Find("Actor").gameObject;
+                GameObject messageText = parent.transform.Find("Message").gameObject;
+                GameObject backgroundBox = parent;
+                backgroundBox.LeanScale(Vector3.one, 0.5f);
+
+                messageText.GetComponent<UnityEngine.UI.Text>().text = dialogo;
+                //Actor actorToDisplay = currentActors[messageToDisplay.actorId];
+                actorName.GetComponent<UnityEngine.UI.Text>().text = actor;
+
+                LeanTween.textAlpha(messageText.GetComponent<UnityEngine.UI.Text>().rectTransform, 0, 0);
+                LeanTween.textAlpha(messageText.GetComponent<UnityEngine.UI.Text>().rectTransform, 1, 0.5f);
+
+
+            }
+            Debug.Log("Dialogo actual " + currentDialogue);
+            if (currentDialogue.Choices[0].Text == "Next Dialogue")
+            {
+                currentDialogue = currentDialogue.Choices[0].NextDialogue;
+                Debug.Log("Siguiente dialogo "+ currentDialogue);
+
+                
+            }
+            else
+            {
+                //Verificar si el siguiente sea decision para mostrarlo inmediatamente
+                DisplayOptions();
+            }
+
+            }
+
+        /*public void NextMessage()
+        {
+            activeMessage++;
+            Debug.Log(activeMessage);
+            Debug.Log(currentMessages);
+            if (activeMessage < currentMessages.Length)
+            {
+                DisplayMessage();
+            }
+            else
+            {
+                Debug.Log("Conversation ended");
+                isActive = false;
+            }
+        }*/
+     
+        public void NextMessage()
+        {
+        
+            if (actor_anterior == "Feña")
+            {
+                GameObject parent = dialogueBoxFenia;
+                //parent.SetActive(false);
+                parent.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
+            } else if (actor_anterior == "Luis")
+            {
+                GameObject parent = dialogueBoxLuis;
+                //parent.SetActive(false);
+                parent.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
+            }
+
+            Debug.Log("Dialogo al teclear " + currentDialogue);
+            if (currentDialogue == null)
+            {
+                Debug.Log("Conversation ended");
+                isActive = false;
+
+                LuisController = Luis.GetComponent<Animator>();
+                SplineFollower splineFollower = Luis.GetComponent<SplineFollower>();
+                splineFollower.enabled = true;
+                LuisController.SetTrigger("caminar_trigger");
+
+                FeniaController = Fenia.GetComponent<Animator>();
+                SplineFollower splineFollowerF = Fenia.GetComponent<SplineFollower>();
+                splineFollowerF.enabled = true;
+                FeniaController.SetTrigger("caminar_trigger");
+
+                LevelLoader.GetComponent<BoxCollider>().enabled = true;
+
+            }
+            else {
+                
+                DisplayMessage();
+            }
+
+        }
+
+
+        public void DisplayOptions()
+        {
+            
+            Debug.Log("Interfaz de decision");
+            isActive = false;
+            decisiones = currentDialogue.Choices.Count;
+
+            for (int i = 0; i < decisiones; i++)
+            {
+                GameObject optionText = options[i].transform.Find("Text").gameObject;
+                optionText.GetComponent<UnityEngine.UI.Text>().text = currentDialogue.Choices[i].Text;
+
+                options[i].SetActive(true);
+                options[i].LeanScale(Vector3.one, 0.3f);
+                
+
+            }
+        }
+
+        public void escogerDecision(int decision)
+        {
+            if (actor_anterior == "Feña")
+            {
+                GameObject parent = dialogueBoxFenia;
+                //parent.SetActive(false);
+                parent.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
+            }
+            else if (actor_anterior == "Luis")
+            {
+                GameObject parent = dialogueBoxLuis;
+                //parent.SetActive(false);
+                parent.LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
+            }
+            Debug.Log("A decidir");
+            currentDialogue = currentDialogue.Choices[decision].NextDialogue;
+            Debug.Log("Decision " + currentDialogue);
+            //Esconder decisiones
+            for (int i = 0; i < decisiones; i++)
+            {
+                //GameObject optionText = options[i].transform.Find("Text").gameObject;
+                //optionText.GetComponent<UnityEngine.UI.Text>().text = currentDialogue.Choices[i].Text;
+                options[i].SetActive(false);
+                options[i].LeanScale(Vector3.zero, 0.5f).setEaseInOutExpo();
+            }
+
+            DisplayMessage();
+            isActive = true;
+
+
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && isActive)
+            {
+                Debug.Log("Acabas de apretar espacio");
+                NextMessage();
+            }
+        }
+    }
+}
